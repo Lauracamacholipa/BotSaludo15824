93a0e3e4f31b58204c2c373ded69be58ed09af84
Laura Camacho <laura.camacho@ucb.edu.bo>
2024-08-17 22:26:40 -0400

    Saluda guardando la informacion del Nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,11 +1,15 @@
 import saludarPorTiempo from "./saludadorConTiempo";
 
-//const inputTexto = document.querySelector("#input-texto");
+const inputTexto = document.querySelector("#input-texto");
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
 
+let estadoConversacion = 0;
+let nombreUsuario;
+
 const saludoInicial = saludarPorTiempo();
 agregarMensajeAlChat("Máquina", saludoInicial);
+agregarMensajeAlChat("Máquina", "Puedes darme tu nombre?");
 
 function agregarMensajeAlChat(origen, mensaje) {
   const mensajeElemento = document.createElement("p");
@@ -15,5 +19,18 @@ function agregarMensajeAlChat(origen, mensaje) {
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+  const textoIngresado = inputTexto.value;
+
+  if (estadoConversacion === 0) {
+    nombreUsuario = textoIngresado;
+    agregarMensajeAlChat("Usuario", nombreUsuario);
+    agregarMensajeAlChat("Máquina", `Gracias, ${nombreUsuario}!`);
+
+    inputTexto.placeholder = "Ingresa tu género...";
+    inputTexto.value = "";
+
+    estadoConversacion = 1;
+    agregarMensajeAlChat("Máquina", "Cual es tu género?");
+  }
 
 });
